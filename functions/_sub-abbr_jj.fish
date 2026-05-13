4a759bc760d83ed4bd7f070b5c32db7fb9e4f665
Drazape <Drazape@outlook.com>
2026-05-13 14:34:16 +0530

    DRYer packages

--- functions/_sub-abbr_jj.fish
+++ functions/_sub-abbr_jj.fish
@@ -1,7 +1,11 @@
-function _sub-abbr_jj --description='subcommand expansions for Jujutsu' --on-event=sub-abbr{s,_jj}
-    set --function sub_abbr sub-abbr jj
-    $sub_abbr b{,ookmark}
-    $sub_abbr c{i,ommit}
-    $sub_abbr desc{,ribe}
-    $sub_abbr st{,atus}
+begin
+    set --local exec_name jj
+    set --local exec_section _{$exec_name}
+    function _sub-abbr{$exec_section} --description='Sub-Command expansions for Jujutsu' --on-event=sub-abbr{s,$exec_section} --inherit-variable=exec_name
+        set --function sub_abbr sub-abbr {$exec_name}
+        $sub_abbr b{,ookmark}
+        $sub_abbr c{i,ommit}
+        $sub_abbr desc{,ribe}
+        $sub_abbr st{,atus}
+    end
 end
--- functions/_sub-abbr_nh.fish
+++ functions/_sub-abbr_nh.fish
@@ -1,6 +1,10 @@
-function _sub-abbr_nh --description='subcommand expansions for nh' --on-event=sub-abbr{s,_nh}
-    # os: allow root
-    for subCommand in switch boot build{,-image,-vm} rollback test
-        sub-abbr --set-cursor 'nh os' {$subCommand}{,' % --bypass-root-check'}
+begin
+    set --local exec_name nh
+    set --local exec_section _{$exec_name}
+    function _sub-abbr{$exec_section} --description='Sub-Command expansions for nh' --on-event=sub-abbr{s,$exec_section} --inherit-variable=exec_name
+        # os: allow root
+        for subCommand in switch boot build{,-image,-vm} rollback test
+            sub-abbr --set-cursor {$exec_name}\ os {$subCommand}{,' % --bypass-root-check'}
+        end
     end
 end
